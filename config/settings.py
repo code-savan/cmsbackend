35a07005a6f9851447e61c9b7565a3eda1ddc9dc
code-savan <eric.marvelboy@gmail.com>
2025-02-26 19:56:20 +0100

    updated sum shi

--- config/settings.py
+++ config/settings.py
@@ -47,9 +47,10 @@ CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
+    "https://cmspdf.vercel.ap"
 ]
 
-CSRF_TRUSTED_ORIGINS = ["http://localhost:5173"]
+CSRF_TRUSTED_ORIGINS = ["http://localhost:5173","https://cmspdf.vercel.app"]
 
 CORS_ALLOW_METHODS = [
     'DELETE',
